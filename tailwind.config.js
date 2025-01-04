f11c3ed6861ce55e872edf2d3a7bd05cdae08c94
ahsantanveer7777 <ahsantanveer7777@gmail.com>
2025-01-05 04:54:41 +0500

    Initial commit for landing page

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+// Module given in Tutorial: 
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      spacing: {
+        '300': '300px',
+      },
+    },
+  },
+  plugins: [],
+}
+
+
+// module.exports = {
+//   content: ["./*.html"],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         'landing': "url('/img/Imagebg.png')",  // Add this custom background image key
+//       }
+//     },
+//   },
+//   plugins: [],
+// }
+
+
+
+
+
